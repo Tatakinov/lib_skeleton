12637b02caa89342fcc98ae0c383ef78e779fe5f
Tatakinov <tatakinov@gmail.com>
2023-11-18 08:57:20 +0900

    sample codeを追加。

--- lib.cc
+++ lib.cc
@@ -14,6 +14,11 @@ bool __unload() {
 
 std::string __request(std::string request) {
     sstp::Request req = sstp::Request::parse(request);
+    if (req["Arugment0"]) {
+        sstp::Response res{"SAORI/1.0", 200, "OK"};
+        res["Result"] = req["Argument0"].value();
+        return res;
+    }
     sstp::Response res{"SAORI/1.0", 204, "No Content"};
     return res;
 }
